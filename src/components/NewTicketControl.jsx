7f5d5ee21e0ffe70c7c1b2071e60cb7499fa519d
Dustin Herboldshimer and Neil Bateman <dustin@>
2019-07-08 10:09:58 -0700

    add conditional rendering to display ConfirmationQuestions

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
 
 class NewTicketControl extends React.Component {
 
@@ -8,20 +9,19 @@ class NewTicketControl extends React.Component {
         this.state = {
             formVisibleOnPage: false
         };
-    //  this.handleClick = this.handleClick.bind(this);
+        this.handleTroubleshootingConformation = this.handleTroubleshootingConformation.bind(this);
+        
         }
-
-    // handleClick(){
-    //     this.setState({formVisibleOnPage: true});
-    //     console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
-    // }
+        handleTroubleshootingConformation(){
+            this.setState({formVisibleOnPage: true});
+        }
 
     render() {
         let currentlyVisibleContent = null;
         if (this.state.formVisibleOnPage){
             currentlyVisibleContent = <NewTicketForm />;
         } else {
-            currentlyVisibleContent = <ConfirmationQuestions />;
+            currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConformation}/>;
         }
         return (
            <div>{currentlyVisibleContent}</div>
